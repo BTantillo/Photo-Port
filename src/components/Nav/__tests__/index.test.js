019730b706d9b1a2dc8172e8aba75f2819b25944
Mountaineer Ben <bentantillo@gmail.com>
2021-12-15 13:28:48 -0800

    all tests passed

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -14,7 +14,7 @@ describe('Nav component', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
 
-    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot(); 
   });
 })
 
