556a3dfebc207cfe94a9d62eb887b1a733170560
Mountaineer Ben <bentantillo@gmail.com>
2021-12-14 16:41:25 -0800

    tests added

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -2,7 +2,6 @@ import react from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 describe('About component', () => {
     // First Test
